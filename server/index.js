35c60151c2f3059c89bc9a781aaf3aec2392968b
Leah <leahlin912@gmail.com>
2019-05-24 15:29:39 +0800

    update multiple clients chatting and updating info in real time function

--- server/index.js
+++ server/index.js
@@ -3,52 +3,80 @@ var wss = new webSocketServer({ port: 5001});
 
 var count = 0;
 wss.on('connection',function(ws){
+    var user = '';
     count++;
     console.log('Curent clients:'+count);
     console.log('[Server] connected!')
     ws.on('message', function(data){
         var newData = JSON.parse(data); 
         console.log(`[Server] received ${data}`);
-        newData.type = 'request';
-        ws.send(JSON.stringify(newData),(err)=>{
-            if(err){
-                console.log(`[Server] error: ${err}`);
-            }
-        });
-        var feedback = {
-            type:'response',
-            msg:'We have received your messaage.'
-        };
+        user = newData.user;
 
-        function addMsg(){
-            
-            switch(newData.user){
-                case 'Amy':
-                    feedback.msg = 'Amy, nice to see you!'+feedback.msg;
-                    console.log("[test]"+feedback.msg);
-                    break;
-                case 'Bora':
-                    feedback.msg = 'Hi Bora!'+feedback.msg;
-                    break;
-                case 'Cherry':
-                    feedback.msg ='Hey! Fat Cherry!'+ feedback.msg;
-                    break;
-                default:
-                    break;
+        wss.clients.forEach(client => {
+            if(client == ws){
+                newData.type = 'request';
+            }else{
+                newData.type = 'response';
             }
-        }
-        async function send(){
-            await addMsg();
-            ws.send(JSON.stringify(feedback));
-            console.log(JSON.stringify(feedback));
-        }
-        send();
+            client.send(JSON.stringify(newData),(err)=>{
+                if(err){
+                    console.log(`[Server] error: ${err}`);
+                }
+            });
+        });
+        
+        
+        // var feedback = {
+        //     type:'response',
+        //     msg:'We have received your messaage.'
+        // };
+
+        // function addMsg(){
+            
+        //     switch(newData.user){
+        //         case 'Amy':
+        //             feedback.msg = 'Amy, nice to see you!'+feedback.msg;
+        //             console.log("[test]"+feedback.msg);
+        //             break;
+        //         case 'Bora':
+        //             feedback.msg = 'Hi Bora!'+feedback.msg;
+        //             break;
+        //         case 'Cherry':
+        //             feedback.msg ='Hey! Fat Cherry!'+ feedback.msg;
+        //             break;
+        //         default:
+        //             break;
+        //     }
+        // }
+        // async function send(){
+        //     await addMsg();
+        //     ws.send(JSON.stringify(feedback));
+        //     console.log(JSON.stringify(feedback));
+        // }
+        // send();
     } );
     
     ws.on('close',function(){
         count --;
-        console.log('[Server] Lost a cliebt.');
-        console.log('Curent clients:'+count);
+        console.log('[Server] Lost a client.');
+        console.log('Online clients:'+count);
+        var date = new Date();
+        newData = {
+            type: 'notify',
+            user: user,
+            msg: '[Left chat room]',
+            time: date.getTime(),
+        }
+        wss.clients.forEach(client => {
+            if(client != ws){
+                client.send(JSON.stringify(newData),(err)=>{
+                    if(err){
+                        console.log(`[Server] error: ${err}`);
+                    }
+                });
+            }
+            
+        });
     })
 
 })
